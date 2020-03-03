dae07dd9adde7b0cf1e89b58d1507a9cf429199c
tootal <tootal@yeah.net>
2020-03-03 15:35:53 +0800

    read index file

--- finder.cpp
+++ finder.cpp
@@ -1,6 +1,13 @@
 #include "finder.h"
 
 #include <QFile>
+#include <QDataStream>
+
+Parser::StringRef *Finder::s_authorIndex = nullptr;
+Parser::StringRef *Finder::s_titleIndex = nullptr;
+quint32 Finder::s_authorIndexs = 0;
+quint32 Finder::s_titleIndexs = 0;
+QFile *Finder::s_file = nullptr;
 
 Finder::Finder(QObject *parent) : QObject(parent)
 {
@@ -10,4 +17,50 @@ Finder::Finder(QObject *parent) : QObject(parent)
 bool Finder::parsed()
 {
     return QFile("author.dat").exists() && QFile("title.dat").exists();
+}
+
+QList<quint32> Finder::indexOfAuthor(const QString &author) const
+{
+    if(s_authorIndex == nullptr){
+        return QList<quint32>{};
+    }
+    
+}
+
+QList<quint32> Finder::indexOfTitle(const QString &title) const
+{
+    if(s_titleIndex == nullptr){
+        return QList<quint32>{};
+    }
+    
+}
+
+void Finder::init()
+{
+    Q_ASSERT(parsed());
+    QFile file("author.dat");
+    file.open(QFile::ReadOnly);
+    Q_ASSERT(file.isOpen());
+    QDataStream stream(&file);
+    s_authorIndexs = static_cast<quint32>(file.size() >> 3);
+    s_authorIndex = new Parser::StringRef[s_authorIndexs];
+    for(quint32 i = 0; i < s_authorIndexs; ++i){
+        stream >> s_authorIndex[i].l >> s_authorIndex[i].r;
+    }
+    file.close();
+    file.setFileName("title.dat");
+    file.open(QFile::ReadOnly);
+    Q_ASSERT(file.isOpen());
+    stream.setDevice(&file);
+    s_titleIndexs = static_cast<quint32>(file.size() >> 3);
+    s_titleIndex = new Parser::StringRef[s_titleIndexs];
+    for(quint32 i = 0; i < s_titleIndexs; ++i){
+        stream >> s_titleIndex[i].l >> s_titleIndex[i].r;
+    }
+    file.close();
+    if(s_file != nullptr){
+        if(s_file->isOpen()) s_file->close();
+        delete s_file;
+        s_file = nullptr;
+    }
 }
--- finder.h
+++ finder.h
@@ -3,12 +3,24 @@
 
 #include <QObject>
 
+#include "parser.h"
+
 class Finder : public QObject
 {
     Q_OBJECT
 public:
     explicit Finder(QObject *parent = nullptr);
-    bool parsed();
+    static bool parsed();
+    static void init();
+    QList<quint32> indexOfAuthor(const QString &author) const;
+    QList<quint32> indexOfTitle(const QString &title) const;
+    
+private:
+    static QFile *s_file;
+    static Parser::StringRef *s_authorIndex;
+    static Parser::StringRef *s_titleIndex;
+    static quint32 s_authorIndexs;
+    static quint32 s_titleIndexs;
 };
 
 #endif // FINDER_H
