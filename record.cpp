83ac586ac1e254ea12f0f4987edf9888406e7351
tootal <tootal@yeah.net>
2020-03-04 09:04:29 +0800

    remove xmlstreamreader

--- record.cpp
+++ record.cpp
@@ -6,34 +6,7 @@
 Record::Record(const QString &s, QObject *parent)
     : QObject(parent)
 {
-    QXmlStreamReader reader(s);
-    while(!reader.isStartElement()) reader.readNext();
-    m_name = reader.name().toString();
-    auto attrs = reader.attributes();
-    Q_ASSERT(attrs.hasAttribute("key"));
-    m_key = attrs.value("key").toString();
-    Q_ASSERT(attrs.hasAttribute("mdate"));
-    m_mdate = attrs.value("mdate").toString();
     
-    while(!reader.atEnd()){
-        reader.readNext();
-        if(reader.isEndElement() && reader.name() == m_name) break;
-        if(reader.isStartElement()){
-            if(reader.name() == "author"){
-                QString author = reader.readElementText(QXmlStreamReader::IncludeChildElements);
-                m_authors.append(author);
-                coauthors_.append(author);
-            }else if(reader.name() == "title"){
-                m_title = reader.readElementText(QXmlStreamReader::IncludeChildElements);
-            }
-        }
-    }
-    // Ignore error
-//    if(reader.hasError()){
-//        qDebug() << reader.errorString();
-//        qDebug() << reader.lineNumber();
-//    }
-//    Q_ASSERT(!reader.hasError());
 }
 
 QString Record::title() const
