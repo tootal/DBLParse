8347726d55a23ee723b260ccf538850bc0fdeb44
tootal <tootal@yeah.net>
2020-03-27 18:11:25 +0800

    sort result

--- resources/index.js
+++ resources/index.js
@@ -1,5 +1,5 @@
 function $(id) {
-    if(typeof id == 'string'){
+    if(typeof id == 'string') {
         if(id[0] == '#') {
             return document.getElementById(id.substr(1));
         }
@@ -20,18 +20,34 @@ var ele_search = $('#search');
 
 var search = function() {
     if(ele_type.value == 'title' && 
-        disable_title_word.indexOf(word.value) != -1){
+        disable_title_word.indexOf(word.value) != -1) {
             ele_result.innerHTML = 'NOT FOUND!';
         }
     else {
         finder.find(ele_word.value, ele_type.value);
     }
-}
+};
 
 var searchAuthor = function(authorEle){
     ele_type.value = 'author';
     ele_word.value = authorEle.innerHTML;
     search();
+};
+
+var formatTitle = function(record) {
+    return '<a href="dblp://key/'+ record.key +'">' + record.title + '</a>';
+};
+
+var formatAuthors = function(record) {
+    let ref = record.authors;
+    for(let j = 0; j < ref.length; ++j) {
+        if(ele_type == 'title' || ref[j] != ele_word.value){
+            ref[j] = '<span class="search-author-other" onclick="searchAuthor(this)">' + ref[j] + '</span>';
+        }else{
+            ref[j] = '<span class="search-author">' + ref[j] + '</span>';
+        }
+    }
+    return ref.join('; ');
 }
 
 var handleSearch = function(data) {
@@ -43,29 +59,35 @@ var handleSearch = function(data) {
 
     if(ele_type.value == 'coauthor') {
         ele_result.innerHTML = '<tr> <th>Co-Author(s)</th> </tr>';
-        for(var i = 0; i < json.length; ++i){
+        for(let i = 0; i < json.length; ++i){
             ele_result.innerHTML += '<td class="search-author-other" onclick="searchAuthor(this)">' + json[i] + '</td>';
         }
-        return ;
-    }
-    ele_result.innerHTML = '<tr> <th>Title</th> <th>Author(s)</th> <th>Year</th> </tr>';
-
-    for(var i = 0; i < json.length; ++i){
-        var tr = $('<tr>');
-        tr.innerHTML += '<td> <a href="dblp://key/'+ json[i].key +'">' + json[i].title + '</a> </td>';
-        let ref = json[i].authors;
-        for(var j = 0; j < ref.length; ++j){
-            if(ele_type == 'title' || ref[j] != ele_word.value){
-                ref[j] = '<span class="search-author-other" onclick="searchAuthor(this)">' + ref[j] + '</span>';
-            }else{
-                ref[j] = '<span class="search-author">' + ref[j] + '</span>';
-            }
+    }else if(ele_type.value == 'title') {
+        ele_result.innerHTML = '<tr> <th>Title</th> <th>Author(s)</th> <th>Modified</th> </tr>';
+        json.sort(function(x, y) {
+            return parseInt(x.mdate) - parseInt(y.mdate);
+        });
+        for(let i = 0; i < json.length; ++i) {
+            let tr = $('<tr>');
+            tr.innerHTML += '<td>' + formatTitle(json[i]) + '</td>';
+            tr.innerHTML += '<td>' + formatAuthors(json[i]) + '</td>';
+            tr.innerHTML += '<td>' + json[i].mdate + '</td>';
+            ele_result.appendChild(tr);
         }
-        tr.innerHTML += '<td>' + json[i].authors.join('; ') + '</td>';
-        tr.innerHTML += '<td>' + json[i].year + '</td>';
-        ele_result.appendChild(tr);
+    }else if(ele_type.value == 'author') {
+        ele_result.innerHTML = '<tr> <th>Title</th> <th>Author(s)</th> <th>Year</th> </tr>';
+        json.sort(function(x, y) {
+            return parseInt(x.year) - parseInt(y.year);
+        });
+        for(let i = 0; i < json.length; ++i){
+            let tr = $('<tr>');
+            tr.innerHTML += '<td>' + formatTitle(json[i]) + '</td>';
+            tr.innerHTML += '<td>' + formatAuthors(json[i]) + '</td>';
+            tr.innerHTML += '<td>' + json[i].year + '</td>';
+            ele_result.appendChild(tr);
+        }
     }
-}
+};
 
 new QWebChannel(qt.webChannelTransport, function(channel) {
     finder = channel.objects.finder;
