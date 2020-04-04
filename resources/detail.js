1776e82ec926116740ad825af9253fc61b158c90
tootal <tootal@yeah.net>
2020-04-04 11:34:16 +0800

    stylish

--- resources/detail.js
+++ resources/detail.js
@@ -1,27 +1,40 @@
-var formatValue = function(str) {
-    let e = new RegExp('(https?|ftp)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]');
-    if(e.test(str)){
-        return '<a href="' + str + '">' + str + '</a>';
-    }else{
-        return str;
-    }
-};
+let items = [
+    'authors',
+    'mdate',
+    'journal',
+    'number',
+    'volume',
+    'pages',
+    'year',
+    'month',
+    'booktitle',
+    'series',
+    'note',
+    'address',
+    'cdrom',
+    'publisher',
+    'crossref',
+    'isbn',
+    'school',
+    'chapter'
+];
 
 var stylish = function(data) {
     let json = JSON.parse(data);
-    // console.log(json);
+    $('#name').innerText = json.name;
     $('#title').innerHTML = json.title;
-    $('#name').innerHTML = json.name;
-    for(let key in json){
-        if(key == 'name') continue;
-        $('#list').innerHTML += '<li><span class="key">' + tr(key) + ': </span>' + formatValue(json[key]) + '</li>';
+    
+    for (let item of items) {
+        if (item in json) {
+            $('#list').innerHTML += `<li class="list-group-item"><span class="font-weight-bold mr-1">${tr(item)}: </span>${json[item]}</li>`;
+        }
     }
 };
 
-if(location.href.startsWith('file:')) {
+if (location.href.startsWith('qrc:')) {
+    stylish($('#src').innerText);
+} else {
     $.load('detail.test.js', function() {
         stylish(test.data);
     });
-} else {
-    stylish($('#src').innerText);
 }
