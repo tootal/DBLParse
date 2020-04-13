2e94a5f6c3e09d2404d1ce808446d4f304f329d5
tootal <tootal@yeah.net>
2020-04-13 22:37:03 +0800

    update resources script

--- scripts/resources.bat
+++ scripts/resources.bat
@@ -1,5 +1,6 @@
 @echo off
 set file=DBLParse.qrc
+cd ..
 echo ^<RCC^>^<qresource prefix="/"^> > %file%
 for %%i in (resources\*) do echo ^<file^>%%i^</file^> >> %file% 
 echo ^</qresource^>^</RCC^> >> %file% 
