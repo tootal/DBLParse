44a4773e8579d995ce6a4f560a54528adcc6177a
tootal <tootal@yeah.net>
2020-04-20 13:32:34 +0800

    update build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,6 +2,6 @@
 echo Setting up environment for Qt usage...
 set PATH=C:\Qt\Qt5.12.7\5.12.7\msvc2017_64\bin;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-
-echo hello
-pause
+qmake --version
+qmake -r 
+nmake
