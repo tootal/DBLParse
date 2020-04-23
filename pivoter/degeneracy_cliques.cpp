c8c59981652fe87d3cb36742bb8b3e1bbdf2c327
tootal <tootal@yeah.net>
2020-04-23 18:39:27 +0800

    remove pivoter type

--- pivoter/degeneracy_cliques.cpp
+++ pivoter/degeneracy_cliques.cpp
@@ -1,6 +1,7 @@
 #include "LinkedList.h"
 #include "MemoryManager.h"
 #include "misc.h"
+#include "degeneracy_cliques.h"
 
 int degeneracyCliques(char *fname) {
     // number of vertices
@@ -8,12 +9,6 @@ int degeneracyCliques(char *fname) {
     
     // 2x number of edges
     int m;
-    
-    // type: A/V/E. 
-    // A for just k-clique information, 
-    // V for per-vertex k-cliques, 
-    // E for per-edge k-cliques
-    char t = 'A'; 
     
     // max_clique_size: max_clique_size. 
     // If 0, calculate for all k.
@@ -24,7 +19,7 @@ int degeneracyCliques(char *fname) {
     
     LinkedList** adjacencyList = readInGraphAdjListToDoubleEdges(&n, &m, fname);
     populate_nCr();
-    runAndPrintStatsCliques(adjacencyList, n, fname, t, max_k, flag_d);
+    runAndPrintStatsCliques(adjacencyList, n, fname, 'A', max_k, flag_d);
     
     int i = 0;
     while (i<n) {
