a625a25182e369233b29c7b47660915349b5af92
tootal <tootal@yeah.net>
2020-08-23 22:08:00 +0800

    build: update workflows

--- scripts/download.py
+++ scripts/download.py
@@ -1,11 +1,13 @@
 import requests
 from clint.textui import progress
+from sys import argv
 
-url = 'https://dblp.org/xml/release/dblp-2020-05-01.xml.gz'
+name = 'dblp-' + argv[1] + '.xml.gz'
+url = 'https://dblp.org/xml/release/' + name
 res = requests.get(url, stream=True)
 total_length = int(res.headers.get('content-length'))
 chunk_size = 10 * 1024 * 1024
-with open("dblp-2020-05-01.xml.gz", "wb") as pypkg:
+with open(name, "wb") as pypkg:
     for chunk in progress.bar(res.iter_content(chunk_size=chunk_size), expected_size=(total_length/chunk_size) + 1, width=100):
         if chunk:
             pypkg.write(chunk)
