9cb74aa2a50d3740272d3bfd4170da64224fdda5
tootal <tootal@yeah.net>
2021-06-29 21:47:09 +0800

    feat: move web resource as submodule

--- src/web/authorStac.test.js
+++
@@ -1,9 +0,0 @@
-var test = {
-    data: "[\n\
-            {\n        \"articleNum\": \"1849\",\n        \"author\": \"H. Vincent Poor\"\n    },\n\
-            {\n        \"articleNum\": \"1463\",\n        \"author\": \"Mohamed-Slim Alouini\"\n    },\n\
-            {\n        \"articleNum\": \"1381\",\n        \"author\": \"Philip S. Yu\"\n    },\n\
-            {\n        \"articleNum\": \"1362\",\n        \"author\": \"Wei Zhang\"\n    },\n\
-            {\n        \"articleNum\": \"1296\",\n        \"author\": \"Wei Wang\"\n    }\
-        ]\n"
-};
